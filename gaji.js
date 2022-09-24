67e63c576b275e4e79713db05e8f83de191d3e07
ikhwanmachmud <75615804+ikhwanmachmud@users.noreply.github.com>
2022-09-24 23:15:22 +0800

    Delete gaji

---
+++ gaji.js
@@ -0,0 +1,5 @@
+const ratePerDay = 112000;
+const workDayInAMonth = prompt("enter your working days");
+const takeHomePay = ratePerDay * workDayInAMonth;
+
+console.log(workDayInAMonth);
--- index.js
+++ index.js
@@ -2,17 +2,16 @@ let answer = "";
 
 for (let number = 0; number < 50; number++) {
   let currentNumber = number;
-  
 
-  if (number % 3 ===  0 && number % 5 === 0) {
-    currentNumber = "Boom Wow"
-  } else if ( number % 3 ===  0) {
-    currentNumber = "Boom"
+  if (number % 3 === 0 && number % 5 === 0) {
+    currentNumber = "Boom Wow";
+  } else if (number % 3 === 0) {
+    currentNumber = "Boom";
   } else if (number % 5 === 0) {
-    currentNumber = "Wow"
-}
+    currentNumber = "Wow";
+  }
 
- answer = answer + `${currentNumber}, `;   
+  answer = answer + `${currentNumber}, `;
 
- console.log(answer);
+  console.log(answer);
 }
